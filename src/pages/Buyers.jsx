5a0d3e80bdf7d86a6c756f2da302c24d0c571aea
adegoke bestman <adegokebestman@gmail.com>
2023-06-25 09:08:50 +0100

    buyer

--- src/pages/Buyers.jsx
+++ src/pages/Buyers.jsx
@@ -1,4 +1,15 @@
+import {AiOutlinePlus} from 'react-icons/ai'
 const Buyers = () => {
-	return <div>Buyers</div>;
+	return (
+	<div>
+	<div className="flex justify-between">
+		<h1 className='font-semibold text-xl'>Buyers Details</h1>
+		<button className="bg-primary-700 px-4 py-2  text-white inline-flex items-center space-x-2 rounded">
+		<AiOutlinePlus className='text-white'/>
+        <span>Create a User Account</span>
+    </button>
+	</div>
+	</div>
+	);
 };
 export default Buyers;
